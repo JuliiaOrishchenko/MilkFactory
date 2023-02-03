73e479660dbf2364dfdf146b5c470a52673e4af7
Yuliia Orishchenko <orishchenko.ya.com>
2023-02-03 14:25:48 +0200

    video#8

--- handlers/admin.py
+++ handlers/admin.py
@@ -5,6 +5,7 @@ from aiogram import types, Dispatcher
 from create_bot import dp, bot
 from data_base import sqlite_db
 from keyboards import admin_kb
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 ID = None
 
@@ -82,6 +83,22 @@ async def load_price(message: types.Message, state: FSMContext):
 
         await sqlite_db.sql_add_command(state)
         await state.finish()
+
+
+@dp.callback_query_handler(lambda x: x.data and x.data.startswith('del '))
+async def del_call_run(callback_query: types.CallbackQuery):
+    await sqlite_db.sql_delete_command(callback_query.data.replace('del ', ''))
+    await callback_query.answer(text=f'{callback_query.data.replace("del ", "")} видалена.', show_alert=True)
+
+
+@dp.message_handler(commands="Delete")
+async def delete_item(message: types.Message):
+    if message.from_user.id == ID:
+        read = await sqlite_db.sql_read2()
+        for ret in read:
+            await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОпис: {ret[2]}\nЦіна {ret[-1]}')
+            await bot.send_message(message.from_user.id, text='^^^', reply_markup=InlineKeyboardMarkup()\
+                                   .add(InlineKeyboardButton(f'Видалити {ret[1]}', callback_data=f'del {ret[1]}')))
 
 
 def register_handlers_admin(dp: Dispatcher):
